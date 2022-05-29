5285d9d3a6c39fa46990153f41a6d3ef3838ea3b
Rikarnto Bariampa <richard1996ba@gmail.com>
2022-05-29 14:56:56 -0700

    Added Movie Theater CRUD

---
+++ UNIWA EEE.2.5.2 Movie Theater Project/Endpoints/CreateMovieTheaterEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using FastEndpoints;
+
+using MovieTheaterProject.Contracts.Requests;
+using MovieTheaterProject.Contracts.Responses;
+using MovieTheaterProject.Mapping;
+using MovieTheaterProject.Services;
+
+namespace MovieTheaterProject.Endpoints;
+
+[HttpPost("/api/movietheaters"), AllowAnonymous]
+public sealed class CreateMovieTheaterEndpoint : Endpoint<CreateMovieTheaterRequest, MovieTheaterResponse>
+{
+    private readonly IMovieTheaterService _movieTheaterService;
+
+    public CreateMovieTheaterEndpoint(IMovieTheaterService movieTheaterService)
+    {
+        _movieTheaterService = movieTheaterService;
+    }
+
+    public override async Task HandleAsync(CreateMovieTheaterRequest req, CancellationToken ct)
+    {
+        var movieTheater = req.ToMovieTheater();
+
+        await _movieTheaterService.CreateAsync(movieTheater);
+
+        var movieTheaterResponse = movieTheater.ToMovieTheaterResponse();
+        await SendCreatedAtAsync<GetMovieTheaterEndpoint>(
+            new { Id = movieTheater.Id.Value }, movieTheaterResponse, generateAbsoluteUrl: true, cancellation: ct);
+    }
+}
